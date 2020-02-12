6ff19ac00cda5dd561535193134a7d4808079857
Bastien Pic <bpic@pcalb-mm0000.univ-jfc.fr>
2020-02-12 11:26:34 +0100

    correction Modele

--- Modele.java
+++ Modele.java
@@ -1,10 +1,10 @@
-
 public class Modele {
 	
-	
-	
-	
-	
+	private int[] COULEURS = {0, 1, 2, 3, 4, 5, 6, 7};
+	// 0->Yellow, 1-> Green, 2->Blue, 3->Magenta , 4->Red, 5->Orange, 6->White, 7->Black
+	private int N_TENTATIVES = 8;
+	private int DIFFICULTE = 4;
+	private enum Etat {EN_COURS, GAGNE, PERDU};
 	
 	
 	
