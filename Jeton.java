adc425adb1ae61e881ab0c16d4a612a7b910b5c1
Bastien Pic <bpic@pcalb-mm0000.univ-jfc.fr>
2020-02-12 11:07:09 +0100

    jeton

---
+++ Jeton.java
@@ -0,0 +1,11 @@
+import java.awt.Color;
+
+public class Jeton {
+	
+	public Color couleur;
+	
+	public Jeton(Color c) {
+		this.couleur = c; 
+	}
+	
+}
---
+++ Rangee.java
@@ -0,0 +1,9 @@
+public class Rangee {
+	
+	
+	
+	public Rangee() {
+		
+	}
+	
+}
